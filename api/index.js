6ac5b26eadf4e61f82a724c8b11d475db44eee87
Eliezer Miguel <eliezermiguel97@outlook.com>
2022-09-09 22:26:33 -0300

    Criacao de migrate e criacao de seeds

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
+const routes = require('./routes');
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-
-const port = 3500;
+routes(app);
 
 app.get('/teste', (req, res) => res.status(200) .send({ mensagem: 'boas-vindas à API' }));
 
